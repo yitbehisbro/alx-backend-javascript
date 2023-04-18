2153a92f4b1fd15a491f1d6bde532c6e05800a46
Yitbarek Wendimu <72982296+yitbehisbro@users.noreply.github.com>
2023-04-18 11:36:49 +0300

    Used ES6 arrow syntax

--- 0x00-ES6_basic/9-getFullBudget.js
+++ 0x00-ES6_basic/9-getFullBudget.js
@@ -4,12 +4,8 @@ export default function getFullBudgetObject(income, gdp, capita) {
   const budget = getBudgetObject(income, gdp, capita);
   const fullBudget = {
     ...budget,
-    getIncomeInDollars() {
-      return `$${income}`;
-    },
-    getIncomeInEuros() {
-      return `${income} euros`;
-    },
+    getIncomeInDollars: () => `$${income}`,
+    getIncomeInEuros: () => `${income} euros`,
   };
 
   return fullBudget;

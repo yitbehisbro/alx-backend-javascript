013924f2543d93cda7e87ccd2bc528e142734cdd
Yitbarek Wendimu <72982296+yitbehisbro@users.noreply.github.com>
2023-04-18 11:44:13 +0300

    Some minor fixes

--- 0x00-ES6_basic/9-getFullBudget.js
+++ 0x00-ES6_basic/9-getFullBudget.js
@@ -4,12 +4,8 @@ const getFullBudgetObject = (income, gdp, capita) => {
   const budget = getBudgetObject(income, gdp, capita);
   const fullBudget = {
     ...budget,
-    getIncomeInDollars() {
-      return `$${income}`;
-    },
-    getIncomeInEuros() {
-      return `${income} euros`;
-    },
+    getIncomeInDollars: (income) => `$${income}`,
+    getIncomeInEuros: (income) => `${income} euros`,
   };
 
   return fullBudget;

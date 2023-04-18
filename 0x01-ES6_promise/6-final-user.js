c405c2eba84cb983ea4f8f6e4db9d74114dea602
Yitbarek Wendimu <72982296+yitbehisbro@users.noreply.github.com>
2023-04-18 20:26:30 +0300

    Minor fixes

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -1,12 +1,19 @@
 import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
-export default function handleProfileSignup(firstName, lastName, fileName) {
-  return Promise.allSettled([
-    signUpUser(firstName, lastName),
-    uploadPhoto(fileName),
-  ]).then((results) => results.map((result) => ({
-    status: result.status,
-    value: result.value || result.reason,
-  })));
+export default async function handleProfileSignup(firstName, lastName, fileName) {
+  const output = [];
+  try {
+    const usersData = await signUpUser(firstName, lastName);
+    output.push({ status: 'fulfilled', value: usersData });
+  } catch (error) {
+    output.push({ status: 'rejected', value: error.toString() });
+  }
+  try {
+    const usersPhoto = await uploadPhoto(fileName);
+    output.push({ status: 'fulfilled', value: usersPhoto });
+  } catch (error) {
+    output.push({ status: 'rejected', value: error.toString() });
+  }
+  return output;
 }

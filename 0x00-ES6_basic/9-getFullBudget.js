3b5db1087d5c7488d389a4f674ea99eea82cfb60
Yitbarek Wendimu <72982296+yitbehisbro@users.noreply.github.com>
2023-04-18 11:45:57 +0300

    Condensing the code

--- 0x00-ES6_basic/9-getFullBudget.js
+++ 0x00-ES6_basic/9-getFullBudget.js
@@ -1,6 +1,6 @@
 import getBudgetObject from './7-getBudgetObject';
 
-const getFullBudgetObject = (income, gdp, capita) => {
+export default function getFullBudgetObject(income, gdp, capita) {
   const budget = getBudgetObject(income, gdp, capita);
   const fullBudget = {
     ...budget,
@@ -10,5 +10,3 @@ const getFullBudgetObject = (income, gdp, capita) => {
 
   return fullBudget;
 };
-
-export default getFullBudgetObject;

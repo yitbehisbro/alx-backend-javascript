0bb62523099899a35ad7ac4d68a277cdb2a14077
Yitbarek Wendimu <72982296+yitbehisbro@users.noreply.github.com>
2023-04-18 11:39:13 +0300

    Used ES6 Arrow Synatx

--- 0x00-ES6_basic/9-getFullBudget.js
+++ 0x00-ES6_basic/9-getFullBudget.js
@@ -1,6 +1,6 @@
 import getBudgetObject from './7-getBudgetObject';
 
-export default function getFullBudgetObject(income, gdp, capita) {
+const getFullBudgetObject = (income, gdp, capita) => {
   const budget = getBudgetObject(income, gdp, capita);
   const fullBudget = {
     ...budget,
@@ -9,4 +9,6 @@ export default function getFullBudgetObject(income, gdp, capita) {
   };
 
   return fullBudget;
-}
+};
+
+export default getFullBudgetObject;

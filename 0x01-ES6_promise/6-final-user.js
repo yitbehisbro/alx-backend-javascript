61b6af6636adbd9485a1bddd06ddeadafa4b5920
Yitbarek Wendimu Oushacho <yitbarekw@hu.edu.et>
2023-04-19 10:52:35 +0000

    Some minor fixes

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -1,11 +1,11 @@
-import signUpUser from './4-user-promise';
-import uploadPhoto from './5-photo-reject';
+import signUpUser from './4-user-promise.js';
+import uploadPhoto from './5-photo-reject.js';
 
 export default function handleProfileSignup(firstName, lastName, fileName) {
-  return Promise.allSettled([
-    signUpUser(firstName, lastName),
-    uploadPhoto(fileName),
-  ]).then((results) => results.map((result) => ({
+  const user = signUpUser(firstName, lastName);
+  const photo = uploadPhoto(fileName);
+
+  return Promise.allSettled([user, photo]).then((results) => results.map((result) => ({
     status: result.status,
     value: result.value || result.reason,
   })));

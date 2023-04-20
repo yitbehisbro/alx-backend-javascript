8d0065f80302d32f01119e40cdb00f89942b0267
Yitbarek Wendimu Oushacho <yitbarekw@hu.edu.et>
2023-04-20 15:44:03 +0000

    Default string representation

---
+++ 0x02-ES6_classes/7-airport.js
@@ -0,0 +1,10 @@
+export default class Airport {
+  constructor(name, code) {
+    this._name = name;
+    this._code = code;
+  }
+
+  toString() {
+    return `Airport [${this._code}] { _name: '${this._name}', _code: '${this._code}' }`;
+  }
+}

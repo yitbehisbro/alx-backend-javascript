0a6158deae5ae16831d0e98aa46dfad22e159929
Yitbarek Wendimu Oushacho <yitbarekw@hu.edu.et>
2023-04-20 16:56:32 +0000

    EVCar

--- 0x02-ES6_classes/100-evcar.js
+++ 0x02-ES6_classes/100-evcar.js
@@ -1,4 +1,4 @@
-import Car from './10-car';
+import Car from './10-car.js';
 
 export default class EVCar extends Car {
   constructor(brand, motor, color, range) {
@@ -6,7 +6,7 @@ export default class EVCar extends Car {
     this._range = range;
   }
 
-  static cloneCar() {
+  cloneCar() {
     return new Car();
   }
 }

3bb7b507d91b707d41defd2f68b88d369ca36c82
Yitbarek Wendimu <72982296+yitbehisbro@users.noreply.github.com>
2023-04-20 14:01:03 +0300

    Implement a function named initializeRooms

--- 0x02-ES6_classes/1-make_classrooms.js
+++ 0x02-ES6_classes/1-make_classrooms.js
@@ -2,12 +2,12 @@ export default class ClassRoom {
   constructor(maxStudentsSize) {
     this._maxStudentsSize = maxStudentsSize;
   }
+}
 
-  static initializeRooms() {
-    return [
-      new ClassRoom(19),
-      new ClassRoom(20),
-      new ClassRoom(34)
-    ];
-  }
+export default function initializeRooms() {
+  return [
+    new ClassRoom(19),
+    new ClassRoom(20),
+    new ClassRoom(34)
+  ];
 }

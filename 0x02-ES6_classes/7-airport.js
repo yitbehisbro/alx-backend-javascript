78acf6932cdc6dab1591fca50f8f7bc364f7d45a
Yitbarek Wendimu Oushacho <yitbarekw@hu.edu.et>
2023-04-20 16:58:32 +0000

    Error fixed

--- 0x02-ES6_classes/7-airport.js
+++ 0x02-ES6_classes/7-airport.js
@@ -5,6 +5,6 @@ export default class Airport {
   }
 
   toString() {
-    return `Airport [${this._code}] { _name: '${this._name}', _code: '${this._code}' }`;
+    return `[object ${this._code}]`;
   }
 }

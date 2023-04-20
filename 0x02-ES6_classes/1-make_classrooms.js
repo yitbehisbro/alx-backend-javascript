a82ff5895b550cbcafc3377b87c409bd69f14e2b
Yitbarek Wendimu Oushacho <yitbarekw@hu.edu.et>
2023-04-20 11:59:19 +0000

    Class importing and function implementation

--- 0x02-ES6_classes/1-make_classrooms.js
+++ 0x02-ES6_classes/1-make_classrooms.js
@@ -1,13 +1,9 @@
-export default class ClassRoom {
-  constructor(maxStudentsSize) {
-    this._maxStudentsSize = maxStudentsSize;
-  }
-}
+import ClassRoom from './0-classroom';
 
 export default function initializeRooms() {
   return [
     new ClassRoom(19),
     new ClassRoom(20),
-    new ClassRoom(34)
+    new ClassRoom(34),
   ];
 }

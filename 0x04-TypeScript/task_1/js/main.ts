b7a8d1801935d26b6cc8dcadec2f23cd6b484f5f
Yitbarek Wendimu <72982296+yitbehisbro@users.noreply.github.com>
2023-04-26 22:23:07 +0300

    Content added

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -41,3 +41,13 @@ const director1: Directors = {
 };
 
 console.log(director1);
+
+interface printTeacherFunction {
+  (firstName: string, lastName: string): string;
+}
+
+const printTeacher: printTeacherFunction = (firstName, lastName) => {
+  return `${firstName[0]}. ${lastName}`;
+};
+
+console.log(printTeacher('John', 'Doe')); // J. Doe
